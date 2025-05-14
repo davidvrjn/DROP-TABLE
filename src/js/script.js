60b354c4093234f0bd39d83e027672dc4e751972
davidvrjn <vanrooijendavid@gmail.com>
2025-05-14 16:22:07 +0200

    Basic Front-End Code Structure
    Added initial frontend code (bootstrap structure) and Parcel config

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+// Import Bootstrap's bundled JavaScript (includes Popper.js)
+import 'bootstrap';
+
+// Add your custom JavaScript code below this line
+console.log('Frontend development script loaded!');
+
+// Example: If you use Bootstrap's tooltips, you might initialize them here
+// var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+// var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+//   return new bootstrap.Tooltip(tooltipTriggerEl)
+// })
