b619c92cd33fb7b3333fe3747742b44fa0f509c7
stephan-kritzinger <u24581705@tuks.co.za>
2025-05-20 08:29:31 +0200

    Added error logging, and skeleton of the getting all products, right now no apikey validation used to retrieve wishlisted products, will be done later

--- API/API.js
+++ API/API.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const mariadb = require('mariadb');
+const fs = require('fs');
 
 //variables for connections
 const port = 3000;
@@ -16,6 +17,42 @@ const pool = mariadb.createPool({
 });
 
 //API CALLS
+//Get products
+app.get('/Get/Products', express.json(), async (req, res) => {
+    let conn;
+    if(!req.is('application/json')){
+        res.status(400).send({
+            status: "Error",
+            message: "Request must be in JSON format, if it is, check request headers are set properly"
+        })
+    }
+    try{
+        conn = await pool.getConnection();
+
+        const rows = await conn.query("SQL QUERY ?, ?", ["param1", "param2"]);
+    }
+    catch(err){
+        console.error(err);
+        fs.appendFileSync(`error.log`, `${new Date().toLocaleString()} - ${err.stack}\n`);
+        res.status(500).send({
+            status: "Error",
+            message: "Error retrieving data, detailed error in server_logs, please investigate server logs"
+        });
+    }
+    finally{
+        if(conn){
+            conn.end();
+        } 
+    }
+})
+
+
+//Error logging
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    fs.appendFileSync(`error.log`, `${new Date().toLocaleString()} - ${err.stack}\n`);
+    res.status(500).send({status: "Error", message: "Error in the API, detailed error in error.log, please investigate error.log"});
+})
 
 //API CONNECT
 app.listen(port, () => {
